7a74efb24f97cc93952265f7c32197e51d2e64c3
Pwako11 <akolo@hotmail.com>
2021-03-30 16:12:03 -0400

    Done.

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ function writeCards(names, eventname){
 }
 writeCards(names, eventname);
 
-// number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-
 function countDown(number) {
         let countDown  = number  
 
