cf72d1536a79fd160d169848c934197b821aa05f
Pwako11 <akolo@hotmail.com>
2021-03-30 16:06:53 -0400

    commit tree 0d15b37bf0e11c63048affe4b5ae69b3c09db196

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+const eventname = 'surprise';
+
+function writeCards(names, eventname){
+  const newArray = new Array();
+  for (let countDown = 0; countDown < names.length; countDown++){
+  newArray.push(`Thank you, ${names[countDown]}, for the wonderful ${eventname} gift!`);
+  console.log(`Thank you, ${names[countDown]}, for the wonderful ${eventname} gift!`);
+  }
+  return newArray;
+}
+writeCards(names, eventname);
+
+// number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+function countDown(number) {
+        let countDown  = number  
+
+        while (countDown > -1) {
+            console.log(countDown--);
+        };
+        // for (let countDown = number.length -1; countDown >= 0; countDown--){
+        // newArray.push(`${number[countDown]}`);
+        // console.log(`${number[countDown]}`);
+        // }
+        // return newArray;
+};
+countDown(number);
